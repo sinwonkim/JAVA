998307660b39f563bcf5db29cba0b5009b1297bd
sinwonkim <zhfps114@gmail.com>
2019-01-04 21:25:23 +0900

    #ArrayList #강화포문 set메소드 사용

--- ReJava/src/ArrayListClass1.java
+++ ReJava/src/ArrayListClass1.java
@@ -4,18 +4,19 @@ public class ArrayListClass1 {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String>  arr = new ArrayList<>();
+		ArrayList<Integer>  arr = new ArrayList<Integer>();
 		
-		arr.add("하나");
-		arr.add("넷");
-		arr.add("둘");
-		arr.add("셋");
+		arr.add(40);
+		arr.add(30);
+		arr.add(10);
+		arr.add(20);
 		
-		for(int i = 0 ; i<arr.size(); i++) { // 
-			
-			System.out.println(arr.get(i));
+		arr.set(2, 20);
+		arr.set(3, 10);
+		for(int e : arr) {
+			System.out.println(e);
 		}
-
+		System.out.println(arr.size());
 	}
 
 }
