8b98f795ac19314298b723ea93160018293abfb7
sinwonkim <zhfps114@gmail.com>
2019-01-27 20:01:20 +0900

    # while문

---
+++ ReJava/src/condition/conditionex.java
@@ -0,0 +1,14 @@
+package condition;
+
+public class conditionex {	
+	public static void main(String[] args) {		
+		int i = 0;
+		int total = 0 ;
+		
+		while(i < 5) {
+		total += i ; 
+			i++;	
+		}
+		System.out.println("1부터 4까지의 합은? "+total+" 입니다.");
+	}
+}
