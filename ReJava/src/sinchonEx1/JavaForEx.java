27ae3fd8975f69c03929f9b88537713de39d1aa0
sinwonkim <zhfps114@gmail.com>
2019-02-13 18:39:29 +0900

    # JAVA 피라미드

---
+++ ReJava/src/sinchonEx1/JavaForEx.java
@@ -0,0 +1,26 @@
+package sinchonEx1;
+
+public class JavaForEx {
+
+	public static void main(String[] args) {
+		int num=5;
+		
+	for(int i = 0; i<num; i++) {
+		for(int s = 0; i<num-s; s++) {
+			System.out.print(" ");
+		}
+		
+		
+		for( int j = 1; j <(i+1)*2; j++) {
+			System.out.print("*");
+			if(i>j) {
+				
+			}
+		}
+		System.out.println();
+		
+	}
+
+	}
+
+}
--- ReJava/src/singletontest/singletonScanner.java
+++ ReJava/src/singletontest/singletonScanner.java
@@ -1,5 +1,4 @@
 package singletontest;
 
 public class singletonScanner {
-
 }
