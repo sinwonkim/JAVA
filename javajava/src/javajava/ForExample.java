9ee235097127a2723049c173c3fc4972843bc37e
sinwonkim <zhfps114@gmail.com>
2019-08-22 01:37:41 +0900

    # 2

---
+++ javajava/src/javajava/ForExample.java
@@ -0,0 +1,17 @@
+package javajava;
+
+public class ForExample {
+	public static void main(String[] args) {
+		//구구단
+		for(int i = 2; i<10; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println("1Loop The end");
+		}
+		// while 문
+		// whlile(조건이 true이면 계속해서 반복한다.){
+		    //실행문;
+	//  }
+	}
+}
---
+++ javajava/src/javajava/RandomMethod.java
@@ -0,0 +1,24 @@
+package javajava;
+
+public class RandomMethod {
+  
+	public static void main(String[] args) {
+		String postion = "과장";
+		
+		switch(postion) {
+		case "과장" :
+			System.out.println("과장 입니다.");
+			break;
+		case "소장" : 
+			System.out.println("소장 입니다.");
+			break;
+		case "신입" :
+			System.out.println("신입 입니다.");
+			break;
+		case "병사" :
+			System.out.println("병사");
+			break;
+		}
+		
+	}
+}
