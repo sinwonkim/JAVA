c8aee22f9a8f99fd24dcc8e3022bae68a06eed29
zhfps <zhfps@LAPTOP-5JB97I4Q>
2019-10-14 23:09:05 +0900

    # Java Exception statement Example

--- JavaExample/src/ExceptionExample.java
+++ JavaExample/src/ExceptionExample.java
@@ -3,7 +3,17 @@ public class ExceptionExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+		try {
+			findCl();
+		} catch (ClassNotFoundException e) {
+			System.out.println("예외 발생함");
+		}
+	}
+		
+		public static void findCl() throws ClassNotFoundException{
+			Class clazz = Class.forName("asdasd");
+		}
 	}
 
-}
