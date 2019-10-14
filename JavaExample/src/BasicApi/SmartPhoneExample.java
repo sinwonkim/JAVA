5d0520a01e9e748535f8afdf62d20b632e5967f1
zhfps <zhfps@LAPTOP-5JB97I4Q>
2019-10-15 00:08:33 +0900

    # 11

--- JavaExample/src/BasicApi/SmartPhoneExample.java
+++ JavaExample/src/BasicApi/SmartPhoneExample.java
@@ -4,6 +4,6 @@ public class SmartPhoneExample {
 	public static void main(String[] args) {
 		SmartPhone smartPhone = new SmartPhone("Samsung","Android");
 		System.out.println("I am using Galaxys8Plus The "
-				+ "operating system of samsungPhone is Android" + smartPhone.toString());
+				+ "operating system of samsungPhone is Android2" + smartPhone.toString());
 	}
 }
