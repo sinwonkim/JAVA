35b2f10c5c678f82caf82206f715aa489f7cb654
zhfps <zhfps@LAPTOP-5JB97I4Q>
2019-11-05 23:41:00 +0900

    2019.11.05 Chapter1 Start Java

--- JavaExT/src/Ex01/Basic01.java
+++ JavaExT/src/Ex01/Basic01.java
@@ -5,6 +5,21 @@ public class Basic01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// 자바 프로그램을 개발하기 위해서는 파일 확장명이 .jva 인 텍스트 파일을 생성하고 소스를 작성한다.
+		// 작성 완료된 자바 소스파일은 컴파일러로 컴파일 해야 한다. -> 컴파일 성공하면 확장명이 .class인 바이트 코드
+		// 파일이 생성된다.  .java 소스 파일 작성 -> 컴파일러(javac.exe)로 바이트 코드 파일(.class) 생성
+		// JVM 구동 명령어(java.exe)로 실행	  
+		/*
+		 * 클래스 : 필드 또는 메소드를 포함하는 블록
+		 * 메소드 : 어떤 일을 처리하는 실행문들을 모아 놓은 블록 
+		 * mainMethod : 프로그램 실행 진입점(entry point)라고 한다.  
+		 * 컴파일된 바이트 코드파일 .class 파일과    자바소스 파일.java  
+		 * 자바소스 파일은 src에   .class 파일은 bin에 있다. 
+		 * 이클립스에서 바이트 코드파일을 실행하는 방법은 Java application을 클릭하면 됨 JVM은 컴파일된 
+		 * 바이트 코드를 실행한다. 
+		 * 
+		
+		 * */
 	}
 
 }
